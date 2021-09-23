694d0860b6d23a8b864f01a64be5d130d6b2744d
Anish Lohiya <anishlohiya196@gmail.com>
2021-09-23 15:53:55 +0530

    Create stack_using_linked_list

---
+++ Stack/stack_using_linked_list.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Stack
+{
+    int data;
+    struct Stack *next;
+};
+
+struct Stack *top = NULL;
+
+void push()
+{
+    int x;
+    printf("Enter Element to be pushed\n");
+    scanf("%d", &x);
+    struct Stack *newNode = (struct Stack *)malloc(sizeof(struct Stack));
+    newNode->data = x;
+    newNode->next = top;
+    top = newNode;
+}
+
+void pop()
+{
+    struct Stack *temp = (struct Stack *)malloc(sizeof(struct Stack));
+    temp = top;
+    if (top == NULL)
+    {
+        printf("Stack is empty\n");
+    }
+    else
+    {
+        printf("Popped Element is %d\n", top->data);
+        top = top->next;
+        free(temp);
+    }
+}
+
+void peek()
+{
+    if (top == NULL)
+    {
+        printf("Stack is empty\n");
+    }
+    else
+    {
+        printf("Top element is %d\n", top->data);
+    }
+}
+
+void display()
+{
+    struct Stack *temp = (struct Stack *)malloc(sizeof(struct Stack));
+    temp = top;
+    if (top == NULL)
+    {
+        printf("Stack is empty\n");
+    }
+    else
+    {
+        while (temp != NULL)
+        {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        }
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int opt;
+    while (1)
+    {
+
+        printf("which operation do you want to perform?\n");
+        printf("1.Push\n");
+        printf("2.Pop\n");
+        printf("3.Peek\n");
+        printf("4.Display\n");
+        printf("5.Exit\n");
+
+        scanf("%d", &opt);
+
+        switch (opt)
+        {
+        case 1:
+            push();
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            peek();
+            break;
+        case 4:
+            display();
+            break;
+        case 5:
+            exit(0);
+            break;
+        default:
+            printf("Unknown operation\n");
+        }
+    }
+    return 0;
+}
