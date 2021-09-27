3113609ea559add3f724f1b248ae931c5e639d67
Anish Lohiya <anishlohiya196@gmail.com>
2021-09-28 02:23:56 +0530

    Add Circular Linked List

---
+++ Linked Lists/Circular Linked List/create_and_display.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *head = NULL;
+struct Node *tail = NULL;
+
+void create()
+{
+    int i, x;
+    printf("How Many Elements You want to add: ");
+    scanf("%d", &x);
+    for (i = 0; i < x; i++)
+    {
+        struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+        printf("Enter data for node %d of the linked list: ", i);
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            newNode->next = head;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+            tail->next = head;
+        }
+    }
+}
+
+void display()
+{
+    struct Node *temp = head;
+    printf("Displaying Circular Linked List\n");
+    if (head == NULL)
+    {
+        printf("Linked List is empty\n");
+    }
+    else
+    {
+        do
+        {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        } while (temp != head);
+    }
+}
+
+int main()
+{
+    create();
+    display();
+    return 0;
+}
