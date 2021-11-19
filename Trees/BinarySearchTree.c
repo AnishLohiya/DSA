f88640a7a89216076fb1d14654f77653f858397e
Anish <87910771+AnishLohiya@users.noreply.github.com>
2021-11-19 17:39:41 +0530

    Update BinarySearchTree.c

--- Trees/BinarySearchTree.c
+++ Trees/BinarySearchTree.c
@@ -1,4 +1,23 @@
 #include<stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    struct Node *left;
+    int data;
+    struct Node *right;
+};
+
+struct Node *root = NULL;
+
+struct Node *findMax(struct Node *root)
+{
+    while (root->right != NULL)
+    {
+        root = root->right;
+    }
+    return root;
+}
 
 int main(){
     
