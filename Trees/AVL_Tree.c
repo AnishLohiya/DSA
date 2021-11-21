3d75ba1304c480a8036d751f715c799a71aa833d
Anish <87910771+AnishLohiya@users.noreply.github.com>
2021-11-22 00:11:17 +0530

    Update AVL_Tree.c

--- Trees/AVL_Tree.c
+++ Trees/AVL_Tree.c
@@ -1,6 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+struct Node
+{
+    struct Node *left;
+    int data;
+    struct Node *right;
+    int height;
+};
+
+struct Node *root = NULL;
+
+struct Node *FindMax(struct Node *root)
+{
+    while (root->right != NULL)
+    {
+        root = root->right;
+    }
+    return root;
+}
+
 int main(){
     
     return 0;
