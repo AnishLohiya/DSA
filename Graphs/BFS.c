79770246d06a2ca3ce0abe12db5073e0a52f43c4
Anish <87910771+AnishLohiya@users.noreply.github.com>
2021-11-25 00:09:27 +0530

    Update BFS.c

--- Graphs/BFS.c
+++ Graphs/BFS.c
@@ -1,4 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int G[][5] = {
+    {0, 1, 1, 1, 0},
+    {1, 0, 1, 0, 0},
+    {1, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0}};
+
+int start = 0, n = 5;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
 
 int main(){
     
