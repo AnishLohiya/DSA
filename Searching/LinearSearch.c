1cd9b6e4ba69cc6235028e509981968b6fa9718e
Anish <87910771+AnishLohiya@users.noreply.github.com>
2021-11-26 00:10:34 +0530

    Update LinearSearch.c

--- Searching/LinearSearch.c
+++ Searching/LinearSearch.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
-void LinearSearch(int *arr, int key)
+int LinearSearch(int *arr, int n, int key)
 {
     int i;
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < n; i++)
     {
         if (arr[i] == key)
         {
             printf("%d found at index %d\n", key, i);
-            return;
+            return i;
         }
     }
     printf("%d not found\n", key);
+    return -1;
 };
 
 int main()
 {
     int a[10] = {10, 12, 3, 41, 5, 16, 77, 87, 9, 100};
     int key = 41;
-    LinearSearch(a, key);
+    int n = sizeof(a) / sizeof(a[0]);
+    LinearSearch(a, n, key);
     return 0;
 }
