3782a78c7e1e54282385f2697e134628d73b6819
Anish <87910771+AnishLohiya@users.noreply.github.com>
2021-11-26 00:11:53 +0530

    Update DFS.c

--- Graphs/DFS.c
+++ Graphs/DFS.c
@@ -1,6 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    
+int G[][5] = {
+    {0, 1, 1, 1, 0},
+    {1, 0, 1, 0, 0},
+    {1, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0}};
+int start = 0, n = 5;
+void DFSTraversal(int G[][5], int start, int n)
+{
+    static int visited[5] = {0};
+    int j;
+    if (visited[start] == 0)
+    {
+        printf("%d ", start);
+        visited[start] = 1;
+        for (j = 1; j < n; j++)
+        {
+            if (G[start][j] == 1 && visited[j] == 0)
+                DFSTraversal(G, j, n);
+        }
+    }
+}
+
+int main()
+{
+    DFSTraversal(G, start, n);
     return 0;
 }
