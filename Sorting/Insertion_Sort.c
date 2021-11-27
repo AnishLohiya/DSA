5f6aba7141e7ce2b141474a6a5421bde9ff59269
Anish <87910771+AnishLohiya@users.noreply.github.com>
2021-11-27 17:52:56 +0530

    Create Insertion_Sort.c

---
+++ Sorting/Insertion_Sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, n, temp;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int a[n];
+
+    printf("\nEnter the elements: ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for (i = 1; i < n; i++)
+    {
+        temp = a[i];
+        j = i - 1;
+        while (j >= 0 && a[j] > temp)
+        {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = temp;
+    }
+
+    printf("\nThe sorted array is: ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
