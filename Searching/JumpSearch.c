d5544734c97b060af0b5a53d174e744f65aacc4f
Anish <87910771+AnishLohiya@users.noreply.github.com>
2021-12-22 01:41:40 +0530

    Create JumpSearch.c

---
+++ Searching/JumpSearch.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+
+int jump_search(int arr[], int n, int jump, int target)
+{
+    int start = 0, end = jump;
+    while (end < n)
+    {
+        if (arr[end] < target)
+        {
+            start = end;
+            end = end + jump;
+        }
+        else
+        {
+            while (start <= end)
+            {
+                if (arr[start] == target)
+                    return start;
+                start++;
+            }
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int target, n, jump;
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    n = sizeof(arr) / sizeof(arr[0]);
+    jump = (int)sqrt(n);
+
+    printf("Enter the element to search: ");
+    scanf("%d", &target);
+
+    int index = jump_search(arr, n, jump, target);
+
+    if (index == -1)
+        printf("Element not found");
+    else
+        printf("Element found at index %d", index + 1);
+    return 0;
+}
